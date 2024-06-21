79603544289c0003f1a55f699ef2fb54c21202d4
kyenideniz <keremefeyenideniz@gmail.com>
2024-06-21 15:47:10 +0300

    auth api problem solved

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,13 +3,15 @@
 import { cn } from "@/lib/utils"
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import {
     SignedIn,
     SignedOut,
     SignInButton,
     UserButton,
   } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 export function MainNav({ className, ...props } : React.HTMLAttributes<HTMLElement>) {
     const pathname = usePathname();
@@ -28,6 +30,10 @@ export function MainNav({ className, ...props } : React.HTMLAttributes<HTMLEleme
         label: 'Routes',
         active: pathname === `/routes`
     },];
+
+    const [open, setOpen] = useState(false);
+    const storeModal = useStoreModal();
+
     return (
         <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
            {routes.map((route, index) => (
@@ -46,6 +52,10 @@ export function MainNav({ className, ...props } : React.HTMLAttributes<HTMLEleme
                 {/* Signed out users get sign in button */}
                 <SignInButton />
             </SignedOut>
+
+            <Button onClick={() => {
+                                setOpen(false); storeModal.onOpen();
+                            }}>add</Button>
             </>
         </nav>
     )
